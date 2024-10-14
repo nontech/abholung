e4728b5689e0865c02e3bd9f1533c0f37fd8760c
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-10-14 22:46:18 +0200

    Tooltip added

--- app/components/ProductInfo.tsx
+++ app/components/ProductInfo.tsx
@@ -4,6 +4,7 @@ import { useState, ChangeEvent } from 'react'
 import Image from 'next/image'
 import type { ProductData } from '../../types/common'
 import TypeOfService from './TypeOfService';
+import { Tooltip } from 'react-tooltip'
 
 interface ProductInfoProps {
   product: ProductData | null;
@@ -84,7 +85,24 @@ export default function ProductInfo({ product, serviceType, onProductFetched, on
 
   return (
     <div>
-      <h1 className="text-xl font-bold mb-4 text-gray-800">eBay Kleinanzeigen Product Link</h1>
+      <div className="flex items-center mb-4">
+        <h1 className="text-xl font-bold text-gray-800">eBay Kleinanzeigen Product Link</h1>
+        <div className="ml-2 text-gray-500 cursor-pointer" data-tooltip-id="product-link-help">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+          </svg>
+        </div>
+        <Tooltip id="product-link-help" place="right" effect="solid">
+          <div className="max-w-xs">
+            <p>To get the product link:</p>
+            <ol className="list-decimal pl-4 mt-2">
+              <li>Go to the eBay Kleinanzeigen product page</li>
+              <li>Copy the URL from your browser's address bar</li>
+              <li>Paste it into the input field below</li>
+            </ol>
+          </div>
+        </Tooltip>
+      </div>
 
       <div className="mb-4 relative">
         <input
