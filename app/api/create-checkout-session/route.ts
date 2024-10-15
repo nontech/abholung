40ceb5298f988aef2371eb39a0212490664623a0
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-10-15 16:33:41 +0200

    Add PayPal

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_types: ['card', 'paypal'],
       line_items: [
         {
           price_data: {
