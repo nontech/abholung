a6635940cc1c51cf04b847b083152a4a519f35aa
Mukesh Jaiswal <jais.mukesh@gmail.com>
2024-10-28 01:03:15 +0100

    Replace product url input with daisyui input

--- app/components/ProductInfo.tsx
+++ app/components/ProductInfo.tsx
@@ -96,16 +96,14 @@ export default function ProductInfo({ onProductFetched, url, onUrlChange, produc
           placeholder="Paste Kleinanzeigen product link here"
           value={url}
           onChange={handleUrlChange}
-          className={`w-full px-4 py-2 pr-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
-            url ? 'text-black font-medium' : 'text-gray-500'
-          }`}
+          className={`input input-bordered w-full ${ url ? 'text-black font-medium' : 'text-gray-500'}`}
           disabled={loading}
         />
         {productError && <p className="text-red-500 text-sm mt-1">{productError}</p>}
         {url && (
           <button
             onClick={clearUrl}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none bg-white z-10"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
